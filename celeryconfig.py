2cde4c2c6e9cae2fc35238674142361ad61c7fc5
achuthvarghese <acvknr@gmail.com>
2022-10-14 00:10:48 +0530

    fix task import

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,5 +1,5 @@
 class CeleryConfig:
     broker_url = "redis://{}:{}/{}".format("redis", 6379, 0)
     result_backend = "redis://{}:{}/{}".format("redis", 6379, 0)
-    # imports = ("executor.tasks", "executor.celery.commands")
+    imports = ("tasks",)
     enable_utc = True
